da4ebe0c29458bd64faad07bbdc61ba78f687033
wordcount <wordcount@163.com>
2019-08-13 05:02:20 -0400

    add driver scripts

--- nvidia/disable_nouveau.sh
+++ nvidia/disable_nouveau.sh
@@ -1,6 +1,5 @@
 sed -i 's/blacklist nvidiafb/#blacklist nvidiafb/' /lib/modprobe.d/dist-blacklist.conf
 echo "blacklist nouveau" >> /lib/modprobe.d/dist-blacklist.conf
 echo "options nouveau modeset=0" >> /lib/modprobe.d/dist-blacklist.conf
-mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img  
-dracut /boot/initramfs-$(uname -r).img $(uname -r)  
-reboot
+mv "/boot/initramfs-$(uname -r).img" "/boot/initramfs-$(uname -r)-nouveau.img"
+dracut "/boot/initramfs-$(uname -r).img" "$(uname -r)"
---
+++ nvidia/driver.sh
@@ -0,0 +1,3 @@
+rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+yum install https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm -y
+yum install kmod-nvidia-430.34 -y
