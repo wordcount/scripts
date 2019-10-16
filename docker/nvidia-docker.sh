a73812c189ce3d47074ea4614352f8488b4d77aa
wordcount <wordcount@163.com>
2019-10-16 03:21:04 -0400

    add docker

---
+++ docker/nvidia-docker.sh
@@ -0,0 +1,5 @@
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | tee /etc/yum.repos.d/nvidia-docker.repo
+yum install -y nvidia-container-toolkit
+ystemctl start docker
+
